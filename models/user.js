1493047cab56396fa046cf603c742ea1e4024564
Julio Bermudez <jbermudezfc90@gmail.com>
2020-05-29 18:48:00 -0400

    change user model

--- models/user.js
+++ models/user.js
@@ -3,7 +3,6 @@ const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
     username: { type: String, required: true, minlength: 5 },
-    email: { type: String, required: true },
     password: { type: String, required: true, minlength: 6 },
     admin: { type: Boolean, required: true },
 });
